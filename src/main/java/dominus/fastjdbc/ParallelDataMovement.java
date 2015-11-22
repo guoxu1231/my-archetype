091c5ac9310ce8a7d1db203bcf639343de828fa4
guoxu1231 <guoxu1231@gmail.com>
2015-11-22 14:04:18 +0800

    fast insert performance test

--- src/main/java/dominus/fastjdbc/ParallelDataMovement.java
+++ src/main/java/dominus/fastjdbc/ParallelDataMovement.java
@@ -1,10 +1,12 @@
 package dominus.fastjdbc;
 
+import dominus.fastjdbc.mongodb.Employee;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
-import sun.nio.ch.ThreadPool;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -15,39 +17,61 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
+ * Local Read Thread MAX
+ * Total Records: 300024 Total Seconds: 36 IOPS:8322/n
+ * Total Records: 300024 Total Seconds: 35 IOPS:8487/n
+ * Total Records: 300024 Total Seconds: 33 IOPS:8847/n
+ * <p/>
+ * <p/>
+ * <p/>
+ *
+ *
+ * [MYSQL INNODB]
  * CDC Write thread MAX IOPS = 18  T[8]=IOPS:18
  * Local Write thread MAX IOPS = 1600  T[8]=IOPS:1555
+ * <p/>
+ * Total Records: 300024 Total Seconds: 205 IOPS:1463/n
+ * Total Records: 300024 Total Seconds: 190 IOPS:1577/n
+ * Total Records: 300024 Total Seconds: 188 IOPS:1593/n
+ * Total Records: 300024 Total Seconds: 200 IOPS:1495/n
+ * <p/>
+ * [innodb_flush_log_at_trx_commit = 0]
+ * Total Records: 300024 Total Seconds: 82 IOPS:3625/n
+ * Total Records: 300024 Total Seconds: 81 IOPS:3680/n
+ * Total Records: 300024 Total Seconds: 81 IOPS:3667/n
+ * Total Records: 300024 Total Seconds: 82 IOPS:3616/n
+ * Total Records: 300024 Total Seconds: 81 IOPS:3701/n
+ * Total Records: 300024 Total Seconds: 78 IOPS:3807/n
  *
- *  Total Records: 300024 Total Seconds: 205 IOPS:1463/n
- *  Total Records: 300024 Total Seconds: 190 IOPS:1577/n
- *  Total Records: 300024 Total Seconds: 188 IOPS:1593/n
- *  Total Records: 300024 Total Seconds: 200 IOPS:1495/n
  *
- *  [innodb_flush_log_at_trx_commit = 0]
- *  Total Records: 300024 Total Seconds: 82 IOPS:3625/n
- *  Total Records: 300024 Total Seconds: 81 IOPS:3680/n
- *  Total Records: 300024 Total Seconds: 81 IOPS:3667/n
- *  Total Records: 300024 Total Seconds: 82 IOPS:3616/n
+ * [MONGODB]
+ * Total Records: 300024 Total Seconds: 98 IOPS:3039/n
+ *
+ *
+ *
+ *
  */
 public class ParallelDataMovement {
 
-    private static BlockingQueue<Object[]> mQueue = new ArrayBlockingQueue<Object[]>(1500);
+    private static BlockingQueue<Object[]> mQueue = new ArrayBlockingQueue<Object[]>(1000);
     private static AtomicInteger totalInsert = new AtomicInteger(0);
     private static Date start;
-    private static int THREAD_SIZE = 8;
+    private static int THREAD_SIZE = 20;
 
     public static void main(String[] args) {
         ApplicationContext context =
-                new ClassPathXmlApplicationContext(new String[]{"jdbc_context.xml"});
+                new ClassPathXmlApplicationContext(new String[]{"jdbc_context.xml", "mongodb_context.xml"});
 
         JdbcTemplate mstemplate = new JdbcTemplate((DataSource) context.getBean("mysql_dataSource"));
         JdbcTemplate mscdctemplate = new JdbcTemplate((DataSource) context.getBean("mysql_dataSource_cdc_iops"));
         JdbcTemplate mslocaltemplate = new JdbcTemplate((DataSource) context.getBean("mysql_dataSource_local_iops"));
         JdbcTemplate otemplate = new JdbcTemplate((DataSource) context.getBean("oracle_dataSource"));
+//        MongoOperations mongoOps = context.getBean(MongoTemplate.class);
 
         otemplate.execute("delete from employees");
         mscdctemplate.execute("delete from employees");
         mslocaltemplate.execute("delete from employees");
+//        mongoOps.dropCollection("employee");
         String total = mstemplate.queryForObject("select count(*) from employees", String.class);
         System.out.println(total);
 
@@ -58,7 +82,7 @@ public class ParallelDataMovement {
             e.printStackTrace();
         }
 
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
         Runnable producer = new RowProducer(mstemplate, "select * from employees order by employees.emp_no asc");
         executor.execute(producer);
 
@@ -66,28 +90,33 @@ public class ParallelDataMovement {
 
         //oracle(cdc) test
         /**
-         for (int i = 0; i < 8; i++) {
+         for (int i = 0; i < THREAD_SIZE; i++) {
          Runnable consumer1 = new RowConsumer(otemplate, "insert into employees(emp_no,birth_date,first_name,last_name,gender,hire_date) values (?,?,?,?,?,?)");
          executor.execute(consumer1);
          }
-         **/
+        **/
 
         //mysql(cdc) test
-        /**
-         for (int i = 0; i < 8; i++) {
+
+         for (int i = 0; i < THREAD_SIZE; i++) {
          Runnable consumer1 = new RowConsumer(mscdctemplate, "insert into employees(emp_no,birth_date,first_name,last_name,gender,hire_date) values (?,?,?,?,?,?)");
          executor.execute(consumer1);
          }
-         **/
+
 
         //mysql(local) test
+        /**
         for (int i = 0; i < THREAD_SIZE; i++) {
-            Runnable consumer1 = new RowConsumer(mslocaltemplate, "insert into employees(emp_no,birth_date,first_name,last_name,gender,hire_date) values (?,?,?,?,?,?)");
+//          Runnable consumer1 = new RowConsumer(mslocaltemplate, "insert into employees(emp_no,birth_date,first_name,last_name,gender,hire_date) values (?,?,?,?,?,?)");
+            Runnable consumer1 = new MongodbRowConsumer(mongoOps);
             executor.execute(consumer1);
         }
+         **/
+
+
         executor.shutdown();
         try {
-            executor.awaitTermination(20,TimeUnit.MINUTES);
+            executor.awaitTermination(20, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -161,6 +190,44 @@ public class ParallelDataMovement {
                 }
                 if (row.length > 0) {
                     template.update(insertSql, row);
+                    totalInsert.incrementAndGet();
+                    System.out.printf("Row is take from queue...%s\n", Thread.currentThread().getName());
+
+                } else {
+                    System.out.printf("[END]-------------------------[END] %s\n", Thread.currentThread().getName());
+                    finished = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * Mongodb Row Consumer
+     */
+    static class MongodbRowConsumer implements Runnable {
+        private MongoOperations mongoOps;
+
+        public MongodbRowConsumer(MongoOperations mongoOps) {
+            this.mongoOps = mongoOps;
+        }
+
+        @Override
+        public void run() {
+            boolean finished = false;
+            while (!finished) {
+                Object[] row = new Object[0];
+                try {
+                    row = mQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (row.length > 0) {
+//                    template.update(insertSql, row);
+                    Employee employee = new Employee((Integer) row[0], new java.util.Date(((java.sql.Date) row[1]).getTime()),
+                            (String) row[2], (String) row[3], (String) row[4], new java.util.Date(((java.sql.Date) row[5]).getTime()));
+                    //class java.lang.Integer,class java.sql.Date,class java.lang.String,class java.lang.String,class java.lang.String,class java.sql.Date
+//                    System.out.printf("%s,%s,%s,%s,%s,%s\n", row[0].getClass(), row[1].getClass(), row[2].getClass(), row[3].getClass(), row[4].getClass(),row[5].getClass());
+                    mongoOps.insert(employee);
                     totalInsert.incrementAndGet();
                     System.out.printf("Row is take from queue...%s\n", Thread.currentThread().getName());
 
