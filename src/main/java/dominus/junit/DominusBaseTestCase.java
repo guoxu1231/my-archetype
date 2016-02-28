4df05720b229daf32b21b1e5517f8547c25ca387
guoxu1231 <guoxu1231@gmail.com>
2016-02-28 10:00:00 +0800

    Sqoop connector test

--- src/main/java/dominus/junit/DominusBaseTestCase.java
+++ src/main/java/dominus/junit/DominusBaseTestCase.java
@@ -5,8 +5,10 @@ import junit.framework.TestCase;
 import org.apache.commons.lang.reflect.FieldUtils;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.PrintStream;
+import java.util.Properties;
 
 
 /**
@@ -18,21 +20,51 @@ public class DominusBaseTestCase extends TestCase {
 
     protected ResourceLoader resourceLoader = new DefaultResourceLoader();
     protected PrintStream out = System.out;
+    protected Properties properties;
+
+    //color stdout
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     @Override
     protected void setUp() throws Exception {
-        out.println("*************************DominusBaseTestCase.setUp***************************");
+        properties = PropertiesLoaderUtils.loadProperties(resourceLoader.getResource("classpath:cdh.properties"));
+        PropertiesLoaderUtils.fillProperties(properties, resourceLoader.getResource("classpath:jdbc.properties"));
+        assertTrue(properties.size() > 0);
+        out.println("[Global Properties]:" + properties.size());
     }
 
     @Override
     protected void tearDown() throws Exception {
-        out.println("*************************DominusBaseTestCase.tearDown************************");
+
+
     }
 
     @Override
     protected void runTest() throws Throwable {
-        out.printf("------------------------------------%s begin------------------------------------\n", super.getName());
+        out.printf("\t\t\t\t\t\t[%s] %s begin\n", this.getClass().getSimpleName(), super.getName());
         super.runTest();
-        out.printf("------------------------------------%s end  ------------------------------------\n", super.getName());
+        out.printf("\t\t\t\t\t\t[%s] %s end\n", this.getClass().getSimpleName(), super.getName());
+    }
+
+    @Override
+    public void runBare() throws Throwable {
+        out.printf(ANSI_CYAN + "*************************[%s] %s.setUp*************************\n", this.getClass().getSimpleName(), super.getName());
+        setUp();
+        out.printf("*************************[%s] %s.setUp*************************\n" + ANSI_RESET, this.getClass().getSimpleName(), super.getName());
+        try {
+            runTest();
+        } finally {
+            out.printf(ANSI_CYAN + "*************************[%s] %s.tearDown*************************\n", this.getClass().getSimpleName(), super.getName());
+            tearDown();
+            out.printf("*************************[%s] %s.tearDown*************************\n" + ANSI_RESET, this.getClass().getSimpleName(), super.getName());
+        }
     }
 }
