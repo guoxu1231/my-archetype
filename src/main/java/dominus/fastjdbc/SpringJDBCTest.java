387e66ee035c4b0d14d33b991f23d14f5221cd0f
guoxu1231 <guoxu1231@gmail.com>
2016-03-05 18:33:20 +0800

    SpringJDBCTest

---
+++ src/main/java/dominus/fastjdbc/SpringJDBCTest.java
@@ -0,0 +1,30 @@
+package dominus.fastjdbc;
+
+
+import com.google.common.io.Files;
+import dominus.junit.DominusBaseTestCase;
+import dominus.junit.annotation.MySqlDataSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
+import org.springframework.jdbc.core.namedparam.ParsedSql;
+
+import java.nio.charset.StandardCharsets;
+
+@MySqlDataSource
+public class SpringJDBCTest extends DominusBaseTestCase {
+
+    String sampleSQL_1;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        sampleSQL_1 = Files.toString(resourceLoader.getResource("classpath:script/sql/sql_sample_1.sql").getFile(), StandardCharsets.UTF_8);
+    }
+
+    public void testNamedParameterUtils() {
+
+        ParsedSql sql = NamedParameterUtils.parseSqlStatement(sampleSQL_1);
+        out.println(sql);
+    }
+
+
+}
