6c056177067e5d68cf8b6fc92122792d6404a203
guoxu1231 <guoxu1231@gmail.com>
2016-03-18 20:28:37 +0800

    KafkaPerformanceTestCase

---
+++ src/main/java/dominus/intg/jms/kafka/KafkaPerformanceTestCase.java
@@ -0,0 +1,25 @@
+package dominus.intg.jms.kafka;
+
+
+import dominus.intg.jms.kafka.producer.KafkaFastProducer;
+import dominus.intg.jms.kafka.producer.KafkaReliableProducer;
+import org.junit.Test;
+import org.springframework.test.annotation.Repeat;
+
+import static org.junit.Assert.assertEquals;
+
+public class KafkaPerformanceTestCase extends KafkaZBaseTestCase {
+
+    @Test
+    @Repeat(50)
+    public void testReliableMessagePublish() throws InterruptedException {
+        long events = Long.valueOf(properties.getProperty("kafka.perf.topic.msgCount"));
+        //KafkaFastProducer  KafkaReliableProducer
+        KafkaFastProducer.main(testTopicName, String.valueOf(events), brokerList);
+        Thread.sleep(5000);//EE: wait for message got flushed in broker
+        //total partition offset should be equal with events count.
+        assertEquals(events, sumPartitionOffset());
+        println(ANSI_RED, "assertEquals passed");
+    }
+
+}
