26965f23a00c9a1a21666013a525661c95618d58
guoxu1231 <guoxu1231@gmail.com>
2016-03-25 15:51:42 +0800

    TestDubboLocalInvoke

--- src/main/java/dominus/intg/rpc/dubbo/TestDubboLocalInvoke.java
+++ src/main/java/dominus/intg/rpc/dubbo/TestDubboLocalInvoke.java
@@ -4,16 +4,18 @@ package dominus.intg.rpc.dubbo;
 import dominus.framework.junit.DominusJUnit4TestBase;
 import dominus.intg.rpc.dubbo.demo.DemoService;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(locations = {"classpath:spring-container/dubbo_consumer_context.xml"})
-public class TestDubboConsumer extends DominusJUnit4TestBase {
+/**
+ * Startup dubbo server in test startup, invoke injvm service provider in each testcase.
+ */
+@ContextConfiguration(locations = {"classpath:spring-container/dubbo_provider_context.xml"})
+public class TestDubboLocalInvoke extends DominusJUnit4TestBase {
 
-    @Autowired
-    DemoService demoService;
 
     @Override
     protected void doSetUp() throws Exception {
@@ -27,9 +29,8 @@ public class TestDubboConsumer extends DominusJUnit4TestBase {
 
     @Test
     public void testInvokeProvider() throws InterruptedException {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:spring-container/dubbo_consumer_context.xml"});
+        DemoService demoService = context.getBean(DemoService.class);
         assertEquals("Hello Shawguo", demoService.sayHello("Shawguo"));
     }
-
-
 }
