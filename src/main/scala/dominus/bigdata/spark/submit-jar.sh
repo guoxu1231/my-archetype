3195109e57834490270c69a9c3ff017f59d538e8
guoxu1231 <guoxu1231@gmail.com>
2016-03-27 10:40:16 +0800

    removed

--- src/main/scala/dominus/bigdata/spark/submit-jar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd /opt/Development/github_repo/archetype-helloworld/target/classes
-rm SparkSalesCount.jar
-jar cf SparkSalesCount.jar dominus/bigdata/spark/SparkSalesCount*.class
-spark-submit --class dominus.bigdata.spark.SparkSalesCount SparkSalesCount.jar
