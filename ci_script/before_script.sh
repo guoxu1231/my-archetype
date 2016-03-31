9333d672fafdbdad2483ef35761e80e0b3432150
guoxu1231 <guoxu1231@gmail.com>
2016-03-31 17:23:48 +0800

    use cached dubbo.

--- ci_script/before_script.sh
+++ ci_script/before_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #Install dubbo dependencies
-git clone https://github.com/alibaba/dubbo.git dubbo
-cd dubbo
-mvn clean install -Dmaven.test.skip -o
+#git clone https://github.com/alibaba/dubbo.git dubbo
+#cd dubbo
+#mvn clean install -Dmaven.test.skip -o
