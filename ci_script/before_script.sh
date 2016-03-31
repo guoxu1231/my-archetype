0e9f0ba1fcb634e07a2ba91e3173d08dd81edde0
guoxu1231 <guoxu1231@gmail.com>
2016-03-31 20:36:20 +0800

    add mysql service to travis ci. import test_db to mysql;

--- ci_script/before_script.sh
+++ ci_script/before_script.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-#Install dubbo dependencies
+#Install dubbo dependencies / cached
 #git clone https://github.com/alibaba/dubbo.git dubbo
 #cd dubbo
 #mvn clean install -Dmaven.test.skip -o
+
+git clone https://github.com/datacharmer/test_db.git test_db
+cd test_db
+mysql < employees.sql
+mysql -t < test_employees_md5.sql
