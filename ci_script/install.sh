8f9d53e4816dbdf3ef4511975d457fea07907ccc
Shawn Guo <guoxu1231@gmail.com>
2016-04-01 13:00:01 +0800

    Update install.sh
    mysql -u root -e "create database IF NOT EXISTS iops_schema;"

--- ci_script/install.sh
+++ ci_script/install.sh
@@ -10,6 +10,7 @@ git clone https://github.com/datacharmer/test_db.git test_db
 cd test_db
 mysql < employees.sql
 mysql -t < test_employees_md5.sql
+mysql -u root -e "create database IF NOT EXISTS iops_schema;"
 mysql -u root -e "show schemas"
 
 #create staging schema
