388814857fa82d124f601b3c1f8ebdda737b1fb9
guoxu1231 <guoxu1231@gmail.com>
2016-04-05 12:41:10 +0800

    integrate mongodb.

--- archetype-helloworld/src/main/java/dominus/intg/datastore/mongodb/MongoIntgTest.java
+++ archetype-helloworld/src/main/java/dominus/intg/datastore/mongodb/MongoIntgTest.java
@@ -1,41 +1,55 @@
 package dominus.intg.datastore.mongodb;
 
 
-import dominus.framework.junit.DominusBaseTestCase;
+import dominus.framework.junit.DominusJUnit4TestBase;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoAdmin;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.Date;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
 
 /**
+ * EE: Local
  * mongodb/bin/mongod --dbpath /opt/Development/middleware/mongodb/data
  */
-public class MongoIntgTest extends DominusBaseTestCase {
+@ContextConfiguration(locations = {"classpath:spring-container/mongodb_context.xml"})
+public class MongoIntgTest extends DominusJUnit4TestBase {
 
-    ApplicationContext context;
+    static String TEST_SCHEMA = "test_schema";
+
+    @Autowired
     MongoOperations mongoOps;
 
+    @Autowired
+    MongoAdmin admin;
+
     @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        context = new ClassPathXmlApplicationContext(new String[]{"mongodb_context.xml"});
-        mongoOps = context.getBean(MongoTemplate.class);
-        assertTrue(mongoOps.collectionExists("iops_schema"));
+    protected void doSetUp() throws Exception {
+        admin.setAuthenticationDatabaseName("admin");
+        admin.createDatabase(TEST_SCHEMA);
+        out.println(admin.getDatabaseStats(TEST_SCHEMA));
     }
 
     @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    protected void doTearDown() throws Exception {
+        admin.dropDatabase(TEST_SCHEMA);
+        out.println(TEST_SCHEMA + " is deleted!");
     }
 
-
+    @Test
     public void testSaveOperation() {
         Employee employee = new Employee(21, new Date(), "shawn", "guo", "M", new Date());
         mongoOps.save(employee);
-        //TODO assert
+        assertEquals("Employee save operation failed", 1, mongoOps.findAll(Employee.class).size());
     }
 
 }
