89a8ad27463db9a107a0c3fe2f13c9763e0dbc3d
guoxu1231 <guoxu1231@gmail.com>
2016-04-23 22:01:14 +0800

    kafka09 producer

---
+++ archetype-helloworld/src/main/java/dominus/intg/jms/kafka09/KafkaProducerTestcase.java
@@ -0,0 +1,195 @@
+package dominus.intg.jms.kafka09;
+
+import dominus.intg.jms.kafka09.sdk.consumer.KafkaConsumerConnector;
+import dominus.intg.jms.kafka09.sdk.producer.KafkaFastProducer;
+import dominus.intg.jms.kafka09.sdk.producer.KafkaReliableProducer;
+import kafka.api.FetchRequest;
+import kafka.api.FetchRequestBuilder;
+import kafka.api.PartitionOffsetRequestInfo;
+import kafka.common.TopicAndPartition;
+import kafka.javaapi.*;
+import kafka.javaapi.consumer.SimpleConsumer;
+import kafka.message.MessageAndOffset;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.junit.Test;
+import org.springframework.test.annotation.Repeat;
+import org.springframework.util.StopWatch;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.*;
+
+/**
+ * EE: Consumer API, Producer API, Admin API
+ * <p/>
+ * [External Dependencies]
+ * CDH Cluster(Kafka Cluster, Zookeeper);
+ * Kafka Test Topic;
+ */
+public class KafkaProducerTestcase extends KafkaZBaseTestCase {
+
+
+    Producer producer;
+
+    @Override
+    protected void doSetUp() throws Exception {
+        super.doSetUp();
+        this.createTestTopic(testTopicName);
+        producer = this.createDefaultProducer(null);
+    }
+
+    @Override
+    protected void doTearDown() throws Exception {
+        producer.close();
+        this.deleteTestTopic(testTopicName);
+        super.doTearDown();
+    }
+
+
+    @Test
+    @Repeat(1)
+    public void testRoundRobinPartitioner() throws InterruptedException {
+        long events = Long.valueOf(properties.getProperty("kafka.test.topic.msgCount"));
+        KafkaFastProducer.main(testTopicName, String.valueOf(events), brokerList);
+        Thread.sleep(5000);
+        //total partition offset should be equal with events count.
+//        assertEquals(events, sumPartitionOffset(brokerList, testTopicName));
+    }
+
+    /**
+     * Test producer.type = async/sync
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    @Repeat(2)
+    public void testKafkaProducer() throws InterruptedException {
+
+        final long events = Long.valueOf(properties.getProperty("kafka.test.topic.msgCount"));
+
+        KafkaConsumerConnector.main(testTopicName, groupId, properties.getProperty("zkQuorum"),
+                properties.getProperty("kafka.test.topic.partition"));
+        //junit.framework.AssertionFailedError:Expected :500 Actual :244
+        Thread.sleep(5000);
+        new Thread() {
+            @Override
+            public void run() {
+                KafkaFastProducer.main(testTopicName, String.valueOf(events), brokerList);
+            }
+        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                KafkaReliableProducer.main(testTopicName, String.valueOf(events), brokerList);
+            }
+        }.start();
+
+        KafkaConsumerConnector.shutdownThread.join();
+        assertEquals("client produced != server offset", events * 2, sumPartitionOffset(brokerList, testTopicName));
+        assertEquals("produced != consumed", events * 2, KafkaConsumerConnector.count.intValue());
+        KafkaConsumerConnector.count.set(0);
+    }
+
+    /**
+     * https://cwiki.apache.org/confluence/display/KAFKA/0.8.0+SimpleConsumer+Example
+     * EE: Consumer message from leader broker, You must keep track of the offsets in your application to know where you left off consuming.
+     * (SimpleConsumer does not require zookeeper to work)
+     */
+    @Test
+    public void testSimpleConsumer() throws UnsupportedEncodingException {
+
+        int kafkaPort = Integer.valueOf(properties.getProperty("kafka.seed.broker.port"));
+        int partitionCount = Integer.valueOf(properties.getProperty("kafka.test.topic.partition"));
+        final int testMsgCount = 5;
+
+        //generate test data
+        KafkaReliableProducer.main(testTopicName, String.valueOf(partitionCount * testMsgCount), brokerList);
+        assertEquals(partitionCount * testMsgCount, sumPartitionOffset(brokerList, testTopicName));
+
+        SimpleConsumer consumer = new SimpleConsumer(properties.getProperty("kafka.seed.broker"), kafkaPort, zkConnectionTimeout, 64 * KB, "leaderLookup");
+        List<String> topics = Collections.singletonList(testTopicName);
+        TopicMetadataRequest req = new TopicMetadataRequest(topics);
+        TopicMetadataResponse resp = consumer.send(req);
+        //EE: find TopicMetadata from seed kafka server
+        List<TopicMetadata> metaData = resp.topicsMetadata();
+        assertTrue(metaData.size() == 1);
+        List<PartitionMetadata> partitionList = metaData.get(0).partitionsMetadata();
+        assertTrue(partitionList.size() >= 1);
+        for (PartitionMetadata partition : partitionList) {
+            println(ANSI_RED, partition);
+            String leadBroker = partition.leader().host();
+            String clientName = "Client_" + testTopicName + "_" + partition.partitionId();
+            SimpleConsumer partitionConsumer = new SimpleConsumer(leadBroker, kafkaPort, zkConnectionTimeout, 64 * KB, clientName);
+            TopicAndPartition topicAndPartition = new TopicAndPartition(testTopicName, partition.partitionId());
+            Map<TopicAndPartition, PartitionOffsetRequestInfo> leastRequestInfo = new HashMap<TopicAndPartition, PartitionOffsetRequestInfo>();
+            leastRequestInfo.put(topicAndPartition, new PartitionOffsetRequestInfo(kafka.api.OffsetRequest.EarliestTime(), 1));
+            Map<TopicAndPartition, PartitionOffsetRequestInfo> latestRequestInfo = new HashMap<TopicAndPartition, PartitionOffsetRequestInfo>();
+            latestRequestInfo.put(topicAndPartition, new PartitionOffsetRequestInfo(kafka.api.OffsetRequest.LatestTime(), Integer.MAX_VALUE));
+            //EE: build OffsetRequest and send it to leadBroker
+            OffsetRequest leastOffsetRequest = new OffsetRequest(leastRequestInfo, kafka.api.OffsetRequest.CurrentVersion(), clientName);
+            OffsetRequest latestOffsetRequest = new OffsetRequest(latestRequestInfo, kafka.api.OffsetRequest.CurrentVersion(), clientName);
+            OffsetResponse leastResponse = partitionConsumer.getOffsetsBefore(leastOffsetRequest);
+            OffsetResponse latestResponse = partitionConsumer.getOffsetsBefore(latestOffsetRequest);
+            assertFalse("Error fetching data Offset Data the Broker. Reason: " + leastResponse.errorCode(testTopicName, partition.partitionId()), leastResponse.hasError());
+            assertFalse("Error fetching data Offset Data the Broker. Reason: " + latestResponse.errorCode(testTopicName, partition.partitionId()), latestResponse.hasError());
+            long leastOffset = leastResponse.offsets(testTopicName, partition.partitionId())[0];
+            long latestOffset = latestResponse.offsets(testTopicName, partition.partitionId())[0];
+            assertEquals(testMsgCount, latestOffset);
+            printf(ANSI_RED, "partition [%d] least offset is %d  latest offset is %d\n", partition.partitionId(), leastOffset, latestOffset);
+
+            FetchRequest fetchRequest = new FetchRequestBuilder()
+                    .clientId(clientName)
+                    .addFetch(testTopicName, partition.partitionId(), leastOffset, 100000) // Note: this fetchSize of 100000 might need to be increased if large batches are written to Kafka
+                    .build();
+            FetchResponse fetchResponse = partitionConsumer.fetch(fetchRequest);
+            //EE: TODO find new leader when met error
+            assertFalse("Error fetching data from the Broker:" + leadBroker + " Reason: " + fetchResponse.errorCode(testTopicName, partition.partitionId()), fetchResponse.hasError());
+
+            //EE: read message from least offset
+            long readOffset = leastOffset;
+            long numRead = 0;
+            for (MessageAndOffset messageAndOffset : fetchResponse.messageSet(testTopicName, partition.partitionId())) {
+                ByteBuffer payload = messageAndOffset.message().payload();
+                byte[] bytes = new byte[payload.limit()];
+                payload.get(bytes);
+                println(ANSI_YELLOW, String.valueOf(messageAndOffset.offset()) + ": " + new String(bytes, "UTF-8"));
+                numRead++;
+            }
+            assertEquals("fetch message count does not match test message count", testMsgCount, numRead);
+            if (partitionConsumer != null) partitionConsumer.close();
+        }
+        if (consumer != null) consumer.close();
+    }
+
+
+    //TODO Throw exception or not? retry two times and throw RuntimeException
+    //TODO Message timeout
+    @Test
+    public void testDefaultProducer() throws InterruptedException, ExecutionException, TimeoutException {
+        long count = Long.valueOf(properties.getProperty("kafka.test.topic.msgCount"));
+        Random rnd = new Random();
+
+        StopWatch watch = new StopWatch("[KafkaSimpleProducer] message count:" + count);
+        watch.start();
+        for (long nEvents = 0; nEvents < count; nEvents++) {
+            long runtime = new Date().getTime();
+            String ip = "192.168.2." + rnd.nextInt(255);
+            String info = runtime + ",www.example.com," + ip;
+            ProducerRecord<String, String> message = new ProducerRecord<String, String>(testTopicName, ip, info);
+
+            RecordMetadata medadata = ((RecordMetadata) producer.send(message).get(10, TimeUnit.SECONDS));
+            out.printf("[Acknowledged Message]:%s, %s, %s\n", medadata.topic(), medadata.partition(), medadata.offset());
+        }
+        watch.stop();
+        System.out.println(watch);
+        assertEquals(count, sumPartitionOffset(brokerList, testTopicName));
+    }
+
+}
---
+++ archetype-helloworld/src/main/java/dominus/intg/jms/kafka09/KafkaZBaseTestCase.java
@@ -0,0 +1,121 @@
+package dominus.intg.jms.kafka09;
+
+
+import dominus.framework.junit.DominusJUnit4TestBase;
+import kafka.admin.AdminUtils;
+import kafka.tools.GetOffsetShell;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * EE: ZK Client
+ * EE: create/delete test topic
+ */
+@ContextConfiguration(locations = "classpath:spring-container/kafka_context.xml")
+public class KafkaZBaseTestCase extends DominusJUnit4TestBase {
+
+    String brokerList;
+    int replicationFactor;
+    String bootstrapServers;
+
+    //ZK
+    static int zkSessionTimeout = 6000;
+    static int zkConnectionTimeout = 10000;
+    ZkUtils zkUtils;
+    ZkClient zkClient;
+
+    //test topic
+    String TEST_TOPIC_PREFIX = "page_visits_";
+    String testTopicName;
+
+    String groupId;
+
+    @Resource(name = "kafkaProducerProps")
+    Properties kafkaProducerProps;
+
+
+    @Override
+    protected void doSetUp() throws Exception {
+        brokerList = properties.getProperty("bootstrap.servers");
+        bootstrapServers = properties.getProperty("bootstrap.servers");
+        replicationFactor = Integer.valueOf(properties.getProperty("kafka.replication.factor"));
+        out.println("[kafka Producer Properties]" + kafkaProducerProps.size());
+        testTopicName = TEST_TOPIC_PREFIX + new Date().getTime();
+        groupId = "dominus.consumer.test." + new Date().getTime();
+
+        // Create a ZooKeeper client
+        // Note: You must initialize the ZkClient with ZKStringSerializer.  If you don't, then
+        // createTopic() will only seem to work (it will return without error).  The topic will exist in
+        // only ZooKeeper and will be returned when listing topics, but Kafka itself does not create the topic.
+        zkClient = new ZkClient(properties.getProperty("zkQuorum"), zkSessionTimeout, zkConnectionTimeout,
+                ZKStringSerializer$.MODULE$);
+        ZkConnection zkConnection = new ZkConnection(properties.getProperty("zkQuorum"));
+        zkUtils = new ZkUtils(zkClient, zkConnection, false);
+    }
+
+    @Override
+    protected void doTearDown() throws Exception {
+        zkUtils.close();
+    }
+
+    protected boolean createTestTopic(String testTopic) {
+        int numPartitions = Integer.valueOf(properties.getProperty("kafka.test.topic.partition"));
+        AdminUtils.createTopic(zkUtils, testTopic, numPartitions, replicationFactor, new Properties());
+        out.printf("Kafka Topic[%s] is created!\n", testTopicName);
+        assertTrue("Kafka Topic[%s] does not exist!", AdminUtils.topicExists(zkUtils, testTopicName));
+        return true;
+    }
+
+    protected boolean deleteTestTopic(String testTopic) {
+        AdminUtils.deleteTopic(zkUtils, testTopicName);
+        out.printf("Kafka Topic[%s] is deleted!\n", testTopicName);
+        return true;
+    }
+
+    //sum all partition offset by using kafka tool(GetOffsetShell)
+    protected static long sumPartitionOffset(String brokerList, String testTopicName) {
+        // Tell Java to use your special stream
+        preCapturedStdout();
+        GetOffsetShell.main(String.format("--broker-list %s --topic %s --time -1", brokerList, testTopicName).split(" "));
+        String output = capturedStdout();
+        if (!StringUtils.hasText(output)) {
+            println(ANSI_RED, "No output from GetOffsetShell!!!");
+            return sumPartitionOffset(brokerList, testTopicName);
+        }
+        println(ANSI_RED, "GetOffsetShell  " + String.format("--broker-list %s --topic %s --time -1 --max-wait-ms 10000", brokerList, testTopicName));
+        println(ANSI_RED, output);
+        long count = 0;
+        for (String partitionOffset : output.split("\n")) {
+            if (StringUtils.hasText(partitionOffset) && partitionOffset.startsWith(testTopicName))
+                count += Integer.valueOf(partitionOffset.split(":")[2]);
+        }
+        return count;
+    }
+
+    /**
+     * Follow Aliyun ONS behaviours.Load props from property file or constant.
+     */
+    protected Producer createDefaultProducer(Properties overrideProps) {
+        kafkaProducerProps.put("bootstrap.servers", bootstrapServers);
+        kafkaProducerProps.put("acks", "all");
+        kafkaProducerProps.put("retries", "2");
+        kafkaProducerProps.put("batch.size", "0");
+        if (overrideProps != null)
+            kafkaProducerProps.putAll(overrideProps);
+//        kafkaProducerProps.list(out);
+        Producer<String, String> producer = new KafkaProducer<>(kafkaProducerProps);
+        return producer;
+    }
+}
