c9b07709b9635e484b9fee840a7acf627c8c9bf5
guoxu1231 <guoxu1231@gmail.com>
2016-04-27 09:48:02 +0800

    compile  error.

--- archetype-helloworld/src/main/java/dominus/bigdata/BigDataTestSuite.java
+++ archetype-helloworld/src/main/java/dominus/bigdata/BigDataTestSuite.java
@@ -4,14 +4,13 @@ package dominus.bigdata;
 import dominus.bigdata.connector.sqoop.SqoopConnectorTest;
 import dominus.bigdata.hdfs.HdfsClientTest;
 import dominus.bigdata.workflow.OozieActionTest;
-import dominus.intg.jms.kafka.KafkaClientTestcase;
-import junit.framework.TestSuite;
+import dominus.intg.jms.kafka09.KafkaProducerTestcase;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 //EE: Aggregating tests in suites
 @RunWith(Suite.class)
-@Suite.SuiteClasses({HdfsClientTest.class, OozieActionTest.class, SqoopConnectorTest.class, KafkaClientTestcase.class})
+@Suite.SuiteClasses({HdfsClientTest.class, OozieActionTest.class, SqoopConnectorTest.class, KafkaProducerTestcase.class})
 public class BigDataTestSuite {
 
     /**JUnit 3.8.x static Test suite() method.
