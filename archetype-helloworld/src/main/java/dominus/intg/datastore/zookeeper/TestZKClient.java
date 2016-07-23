d40687d7c464cebd98332dfca283c0098b8b178a
guoxu1231 <guoxu1231@gmail.com>
2016-07-23 22:30:07 +0800

    testSessionTimeout

--- archetype-helloworld/src/main/java/dominus/intg/datastore/zookeeper/TestZKClient.java
+++ archetype-helloworld/src/main/java/dominus/intg/datastore/zookeeper/TestZKClient.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
