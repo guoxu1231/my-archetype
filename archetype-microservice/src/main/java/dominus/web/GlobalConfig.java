2dcddf65ba7e5ab3b8c48d97ef289f14c50bddc4
guoxu1231 <guoxu1231@gmail.com>
2016-08-07 21:43:47 +0800

    GroovyScriptEndpoint

---
+++ archetype-microservice/src/main/java/dominus/web/GlobalConfig.java
@@ -0,0 +1,11 @@
+package dominus.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "gladiator")
+public class GlobalConfig {
+
+
+}
--- archetype-microservice/src/main/java/dominus/web/WebApplication.java
+++ archetype-microservice/src/main/java/dominus/web/WebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(CdcConfig.class)
+@Import({GlobalConfig.class, CdcConfig.class,})
 public class WebApplication {
 
     public static void main(String[] args) throws Exception {
