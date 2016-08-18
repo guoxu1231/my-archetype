bd08c8223009ebbeb0e8da6cdee82ebacd2c7e95
guoxu1231 <guoxu1231@gmail.com>
2016-08-18 22:34:41 +0800

    testSlowConsumer

--- archetype-helloworld/src/main/java/dominus/intg/jms/rabbitmq/TestRabbitMQClient.java
+++ archetype-helloworld/src/main/java/dominus/intg/jms/rabbitmq/TestRabbitMQClient.java
@@ -2,27 +2,30 @@ package dominus.intg.jms.rabbitmq;
 
 
 import com.rabbitmq.client.*;
+import com.rabbitmq.client.impl.DefaultExceptionHandler;
 import dominus.framework.junit.DominusJUnit4TestBase;
 import dominus.framework.junit.annotation.MessageQueueTest;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.Test;
 import org.springframework.core.annotation.AnnotationUtils;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
 public class TestRabbitMQClient extends DominusJUnit4TestBase {
 
     //abstracts the socket connection, and takes care of protocol version negotiation and authentication and so on
-    Connection conn;
+    Connection pubConn;
+    Connection subConn;
     // most of the API for getting things done resides
-    Channel channel;
+    Channel pubChannel;
+    Channel subChannel;
+    //thread execution service for consumers on the connection
+    ExecutorService executorService;
     static final String QUEUE_NAME = "TEST_RABBITMQ_QUEUE";
     static final String EXCHANGE_NAME = "TEST_RABBITMQ_EXCHANGE";
     static final String ROUTING_KEY = "TEST_ROUTING_KEY";
@@ -31,27 +34,40 @@ public class TestRabbitMQClient extends DominusJUnit4TestBase {
     @Override
     protected void doSetUp() throws Exception {
         super.doSetUp();
-
-        messageQueueAnnotation = AnnotationUtils.getAnnotation(this.getClass().getMethod(this.name.getMethodName()), MessageQueueTest.class);
+        //EE: only one thread in consumer thread pool
+        executorService = Executors.newFixedThreadPool(1);
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(properties.getProperty("rabbitmq.host"));
         factory.setUsername(properties.getProperty("rabbitmq.user"));
         factory.setPassword(properties.getProperty("rabbitmq.user"));
 //        factory.setAutomaticRecoveryEnabled(true);
-        conn = factory.newConnection();
-        channel = conn.createChannel();
-        //
-        channel.exchangeDeclare(EXCHANGE_NAME, "direct", true);
-        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+        pubConn = factory.newConnection();
+        //thread execution service for consumers on the connection
+        subConn = factory.newConnection();
+        pubChannel = pubConn.createChannel();
+        subChannel = subConn.createChannel();
+
+        //cleanup queue
+        pubChannel.queueDelete(QUEUE_NAME);
+
+        //exchanges and queues
+        pubChannel.exchangeDeclare(EXCHANGE_NAME, "direct", true);
+        subChannel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        subChannel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+        sleep(3000);
+
+        messageQueueAnnotation = AnnotationUtils.getAnnotation(this.getClass().getMethod(this.name.getMethodName()), MessageQueueTest.class);
     }
 
     @Override
     protected void doTearDown() throws Exception {
         super.doTearDown();
-        channel.close();
-        conn.close();
+        pubChannel.close();
+        subChannel.close();
+        pubConn.close();
+        subConn.close();
+        executorService.shutdown();
     }
 
     @MessageQueueTest(count = 10000)
@@ -63,7 +79,7 @@ public class TestRabbitMQClient extends DominusJUnit4TestBase {
 
         produceTestMessage(count);
 
-        channel.basicConsume(QUEUE_NAME, false, "myConsumerTag", new DefaultConsumer(channel) {
+        pubChannel.basicConsume(QUEUE_NAME, false, "myConsumerTag", new DefaultConsumer(pubChannel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
                     throws IOException {
@@ -71,15 +87,57 @@ public class TestRabbitMQClient extends DominusJUnit4TestBase {
                 long deliveryTag = envelope.getDeliveryTag();
                 logger.info(" [x] Received {} Tag {}'", message, deliveryTag);
 
-                channel.basicAck(deliveryTag, false);
+                pubChannel.basicAck(deliveryTag, false);
                 latch.countDown();
             }
         });
         assertTrue(latch.await(10, TimeUnit.MINUTES));
     }
 
-    public void testSlowConsumer() {
+    @MessageQueueTest(count = 100000)
+    @Test
+    public void testSlowConsumer() throws IOException, InterruptedException, TimeoutException {
 
+        int count = messageQueueAnnotation.count();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(properties.getProperty("rabbitmq.host"));
+        factory.setUsername(properties.getProperty("rabbitmq.user"));
+        factory.setPassword(properties.getProperty("rabbitmq.user"));
+        factory.setExceptionHandler(new DefaultExceptionHandler() {
+            @Override
+            public void handleConsumerException(Channel channel, Throwable exception, Consumer consumer, String consumerTag, String methodName) {
+                out.printf("%s : %s\n", exception.getClass().getName(), exception.getMessage());
+                assertTrue(exception instanceof java.net.SocketException || exception instanceof AlreadyClosedException);
+                latch.countDown();
+            }
+        });
+        Connection testConn = factory.newConnection(executorService);
+        Channel testChannel = testConn.createChannel();
+
+        produceTestMessage(count);
+        //EE: default prefetch is unlimited. Reduce prefetch count to eliminate connection timeout,
+//        testChannel.basicQos(10);
+
+        testChannel.basicConsume(QUEUE_NAME, false, "myConsumerTag", new DefaultConsumer(testChannel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                long deliveryTag = envelope.getDeliveryTag();
+                out.printf(" [x] Received %s Tag %s\n", message, deliveryTag);
+                testChannel.basicAck(deliveryTag, false);
+                sleep(1000);
+            }
+        });
+        assertTrue(latch.await(10, TimeUnit.MINUTES));
+        try {
+            testChannel.close();
+            testConn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void testCompetingConsumer() {
@@ -95,7 +153,7 @@ public class TestRabbitMQClient extends DominusJUnit4TestBase {
                     String ip = "192.168.2." + random.nextInt(255);
                     String info = runtime + ",www.example.com," + ip;
                     try {
-                        channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, null, info.getBytes());
+                        pubChannel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, null, info.getBytes());
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
