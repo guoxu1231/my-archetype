b280937c1978732f23a1ac0b730986f5edd16509
guoxu1231 <guoxu1231@gmail.com>
2016-08-20 12:01:35 +0800

    add binlog parsing speed stat.

--- archetype-service/src/main/java/gladiator/binlog/BinlogConfig.java
+++ archetype-service/src/main/java/gladiator/binlog/BinlogConfig.java
@@ -10,7 +10,7 @@ public class BinlogConfig extends GlobalConfig {
     @Bean(initMethod = "init", destroyMethod = "cleanup")
     @Description("Run as mysql slave and publish all insert/update/delete events to kafka topics")
     public BinaryLogClientBean binaryLogClientBean() {
-        return new BinaryLogClientBean(env.getProperty("mysql.hostname.cdc"), Integer.valueOf(env.getProperty("mysql.port.cdc")), env.getProperty("mysql.schema.cdc"),
+        return new BinaryLogClientBean(env.getProperty("mysql.hostname.cdc"), Integer.valueOf(env.getProperty("mysql.port.cdc")),
                 env.getProperty("mysql.username.cdc"), env.getProperty("mysql.password.cdc"));
     }
 }
