c3749f80865529f2b7db305e5f0b808a44f3ebad
guoxu1231 <guoxu1231@gmail.com>
2016-08-29 13:50:10 +0800

    multiple writer.

--- archetype-service/src/main/java/gladiator/binlog/BinLogGeneratorService.java
+++ archetype-service/src/main/java/gladiator/binlog/BinLogGeneratorService.java
@@ -3,12 +3,21 @@ package gladiator.binlog;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.Assert;
 import org.springframework.util.StopWatch;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * select all PK into memory;
@@ -20,7 +29,10 @@ public class BinLogGeneratorService {
 
     final static String LOAD_PK_SQL = "select %s from %s";
     final static String UPDATE_TIMESTAMP_SQL = "update %s set %s=? where %s=?";
-    List<Long> pkList;
+    BlockingQueue<Long> pkBlockingQueue;
+    Long total;
+    final AtomicLong count = new AtomicLong(0);
+    AtomicBoolean running = new AtomicBoolean(true);
 
     JdbcTemplate jdbcTemplate;
 
@@ -30,26 +42,61 @@ public class BinLogGeneratorService {
     }
 
     public String loadPK(String table, String pkColumn) {
-        pkList = jdbcTemplate.queryForList(String.format(LOAD_PK_SQL, pkColumn, table), Long.class);
+        List<Long> pkList = jdbcTemplate.queryForList(String.format(LOAD_PK_SQL, pkColumn, table), Long.class);
+        pkBlockingQueue = new ArrayBlockingQueue<>(pkList.size(), false, pkList);
+        total = Long.valueOf(pkList.size());
 
         String echo = String.format("load %s PKs from %s.%s", pkList.size(), table, pkColumn);
         logger.info(echo);
         return echo;
     }
 
-    public String updateTimestamp(String table, String pkColumn, String timestampColumn) {
+    public String updateTimestamp(String table, String pkColumn, String timestampColumn, int concurrency) throws ExecutionException, InterruptedException {
 
-        if (pkList == null || pkList.size() == 0)
+        if (pkBlockingQueue == null || pkBlockingQueue.size() == 0)
             this.loadPK(table, pkColumn);
 
-        StopWatch stopWatch = new StopWatch(String.format("%s.%s update timestamp in %d records", table, pkColumn, pkList.size()));
+        StopWatch stopWatch = new StopWatch(String.format("%s.%s update timestamp in %d records", table, pkColumn, pkBlockingQueue.size()));
         stopWatch.start();
-        for (Long pk : pkList) {
-            jdbcTemplate.update(String.format(UPDATE_TIMESTAMP_SQL,
-                    table, timestampColumn, pkColumn), new Date(), pk);
+
+        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor("binlog-generator-thread-");
+        final AtomicLong stat = new AtomicLong(0);
+        count.set(0);
+        running.set(true);
+        final Long startTime = System.currentTimeMillis();
+        List<Future<Long>> result = new ArrayList<>(concurrency);
+        for (int i = 0; i < concurrency; i++) {
+            result.add(executor.submit(() -> {
+                long localCount = 0;
+                while (!pkBlockingQueue.isEmpty() && running.get()) {
+                    Long pk = pkBlockingQueue.poll();
+                    if (pk != null) {
+                        jdbcTemplate.update(String.format(UPDATE_TIMESTAMP_SQL,
+                                table, timestampColumn, pkColumn), new Date(), pk);
+                        //db profiler
+                        if (stat.getAndIncrement() % 10000 == 0) {
+                            long elapsed = System.currentTimeMillis() - startTime;
+                            logger.info("consumed:{}/total:{}, tps: {}", count.get(), total, (stat.get() * 1000) / elapsed);
+                        }
+                        localCount++;
+                        count.incrementAndGet();
+                    }
+                }
+                logger.info("{} : {}", Thread.currentThread().getName(), localCount);
+                return localCount;
+            }));
         }
+        long assertCount = 0;
+        for (int i = 0; i < concurrency; i++)
+            assertCount += result.get(i).get();
+        Assert.isTrue(running.get() ? assertCount == total : true, String.format("expected:%s,actual:%", total, assertCount));
+
         stopWatch.stop();
         return stopWatch.toString();
     }
 
+    public String stop() {
+        this.running.set(false);
+        return String.format("binlog generator is stopped. %s/%s", count, total);
+    }
 }
