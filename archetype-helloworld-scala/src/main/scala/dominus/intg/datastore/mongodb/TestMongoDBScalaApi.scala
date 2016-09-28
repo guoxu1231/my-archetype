0589a8a0a0a3f333b437ee4ab9ae578141083708
guoxu <guoxu1231@gmail.com>
2016-09-28 11:20:07 +0800

    testObjectTree

--- archetype-helloworld-scala/src/main/scala/dominus/intg/datastore/mongodb/TestMongoDBScalaApi.scala
+++ archetype-helloworld-scala/src/main/scala/dominus/intg/datastore/mongodb/TestMongoDBScalaApi.scala
@@ -1,11 +1,12 @@
 package dominus.intg.datastore.mongodb
 
+import com.mongodb.BasicDBObject
 import com.mongodb.casbah.Imports._
 import dominus.framework.junit.DominusJUnit4TestBase
+import org.junit.Assert._
 import org.junit.Test
 import salat._
 import salat.global._
-import org.junit.Assert._
 
 /**
  * The Casbah driver provides an idiomatic Scala API that is built on top of the MongoDB Async Java driver.
@@ -44,6 +45,29 @@ class TestMongoDBScalaApi extends DominusJUnit4TestBase {
     assertEquals(1, coll.count())
     coll.find().foreach(doc => println(doc))
   }
+
+  @Test
+  def testObjectTree(): Unit = {
+    val coll = mongoClient("test-db")("test-coll")
+
+    //Scala object -> Mongodb DBObject
+    val shanghai = City("Shanghai", List(Location("zhangjiang 2F", List(Room("room1", "room 1 description", "open"),
+      Room("room2", "room 2 description", "closed")))))
+    val dbo1 = grater[City].asDBObject(shanghai)
+    assertTrue(dbo1.isInstanceOf[BasicDBObject])
+    println(dbo1)
+    coll.insert(dbo1)
+    assertEquals(1, coll.count())
+
+    //Mongodb DBObject -> Scala object
+    val dbo2 = coll.findOne(MongoDBObject("name" -> "Shanghai")).getOrElse().asInstanceOf[BasicDBObject]
+    assertTrue(dbo2.isInstanceOf[BasicDBObject])
+    val dbo3 = coll.findOneByID(dbo2.getObjectId("_id")).getOrElse()
+    assertTrue(dbo3.isInstanceOf[BasicDBObject])
+    val city = grater[City].asObject(dbo3.asInstanceOf[BasicDBObject])
+    println(city)
+    assertEquals(shanghai, city)
+  }
 }
 
 case class Employee(name: String, age: Option[Int], annual_salary: Option[BigDecimal])
@@ -52,3 +76,8 @@ case class Department(name: String, head_honcho: Option[Employee], cya_factor: B
 
 case class Company(name: String, year_of_inception: Int, departments: Map[String, Department])
 
+case class Room(name: String, desc: String, status: String)
+
+case class Location(name: String, roomList: List[Room])
+
+case class City(name: String, locationList: List[Location])
