ea1ccb7aba90aefd511e7e635052ecac8eb2e9b9
guoxu1231 <guoxu1231@gmail.com>
2016-11-02 23:58:30 +0800

    TestConsulClient

--- archetype-helloworld/src/main/java/dominus/intg/coordinator/consul/TestConsulClient.java
+++ archetype-helloworld/src/main/java/dominus/intg/coordinator/consul/TestConsulClient.java
@@ -1,25 +1,35 @@
 package dominus.intg.coordinator.consul;
 
 
-import com.orbitz.consul.AgentClient;
-import com.orbitz.consul.Consul;
+import com.orbitz.consul.*;
+import com.orbitz.consul.cache.ServiceHealthCache;
+import com.orbitz.consul.model.agent.Agent;
+import com.orbitz.consul.model.health.ServiceHealth;
 import dominus.framework.junit.DominusJUnit4TestBase;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Required: communicate with remote consul client(-client=0.0.0.0).
+ * https://www.consul.io/docs/agent/http.html
+ */
 public class TestConsulClient extends DominusJUnit4TestBase {
+
+    Consul consul;
 
     @Override
     protected void doSetUp() throws Exception {
         super.doSetUp();
 
-        Consul consul = Consul.builder().withUrl("http://10.253.11.216:8500").build(); // connect to Consul on localhost
-        AgentClient agentClient = consul.agentClient();
+        consul = Consul.builder().withUrl("http://10.253.11.216:8500").build(); // connect to Consul on localhost
 
-        String serviceName = "MyService";
-        String serviceId = "1";
-
-        agentClient.register(8080, 3L, serviceName, serviceId); // registers with a TTL of 3 seconds
-        agentClient.pass(serviceId); // check in with Consul, serviceId required only.  client will prepend "service:" for service level checks.
     }
 
     @Override
@@ -28,7 +38,62 @@ public class TestConsulClient extends DominusJUnit4TestBase {
     }
 
     @Test
-    public void testNull() {
+    public void testServiceRegisterAndCheck() throws NotRegisteredException {
+        AgentClient agentClient = consul.agentClient();
+        String serviceName = "MyService";
+        String serviceId = "1";
 
+        agentClient.register(8080, 30L, serviceName, serviceId); // registers with a TTL of 30 seconds
+        agentClient.pass(serviceId); // check in with Consul, serviceId required only.  client will prepend "service:" for service level checks.
+
+        sleep(2000);
+
+        HealthClient healthClient = consul.healthClient();
+        List<ServiceHealth> nodes = healthClient.getHealthyServiceInstances("MyService").getResponse(); // discover only "passing" nodes
+        assertEquals("serfHealth", nodes.get(0).getChecks().get(0).getCheckId());
+        assertEquals("service:1", nodes.get(0).getChecks().get(1).getCheckId());
+        logger.info(Arrays.toString(nodes.get(0).getChecks().toArray()));
     }
+
+    @Test
+    public void testStoreKV() {
+        KeyValueClient kvClient = consul.keyValueClient();
+        kvClient.putValue("foo", "bar");
+        assertEquals("bar", kvClient.getValueAsString("foo").get());
+    }
+
+    @Test
+    public void testServiceHealthSubscribe() throws Exception {
+        CountDownLatch isDead = new CountDownLatch(1);
+        AgentClient agentClient = consul.agentClient();
+        String serviceName = "MyService2";
+        String serviceId = "1";
+
+        agentClient.register(8080, 5L, serviceName, serviceId); // registers with a TTL of 3 seconds
+        agentClient.pass(serviceId); // check in with Consul, serviceId required only.  client will prepend "service:" for service level checks.
+
+        Agent agent = consul.agentClient().getAgent();
+        HealthClient healthClient = consul.healthClient();
+        ServiceHealthCache svHealth = ServiceHealthCache.newCache(healthClient, serviceName);
+        svHealth.addListener(newValues -> {
+            // do Something with updated server map
+            if (newValues.size() != 0) {
+                out.println(serviceName + " register success!");
+                out.println(newValues.toString());
+            } else {
+                out.println(serviceName + " TTL is over");
+                isDead.countDown();
+            }
+
+        });
+        svHealth.start();
+        assertTrue(isDead.await(1, TimeUnit.MINUTES));
+    }
+
+    @Test
+    public void testBlock() {
+        //TODO
+    }
+
+
 }
