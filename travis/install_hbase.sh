c17b813ad9d2d63a4194f825d42bdb7c867a0a71
guoxu <guoxu1231@gmail.com>
2017-03-27 18:37:47 +0800

    update hbase download url.

--- travis/install_hbase.sh
+++ travis/install_hbase.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 mkdir $HOME/download -p
-if [ ! -f $HOME/download/hbase-1.2.4-bin.tar.gz ]; then sudo wget -O $HOME/download/hbase-1.2.4-bin.tar.gz http://www-eu.apache.org/dist/hbase/1.2.4/hbase-1.2.4-bin.tar.gz; fi
+if [ ! -f $HOME/download/hbase-1.2.4-bin.tar.gz ]; then sudo wget -O $HOME/download/hbase-1.2.4-bin.tar.gz https://archive.apache.org/dist/hbase/1.2.4/hbase-1.2.4-bin.tar.gz; fi
 sudo mv $HOME/download/hbase-1.2.4-bin.tar.gz hbase-1.2.4-bin.tar.gz && tar xzf hbase-1.2.4-bin.tar.gz
 sudo rm -f hbase-1.2.4/conf/hbase-site.xml && sudo mv travis/config/hbase/hbase-site.xml hbase-1.2.4/conf
 sudo hbase-1.2.4/bin/start-hbase.sh
