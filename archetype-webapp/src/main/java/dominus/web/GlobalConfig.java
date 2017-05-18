d5b6c0e982deb3985658137272ccb96cb3e57f79
guoxu <guoxu1231@gmail.com>
2017-05-18 11:37:07 +0800

    add multiple properties.

--- archetype-webapp/src/main/java/dominus/web/GlobalConfig.java
+++ archetype-webapp/src/main/java/dominus/web/GlobalConfig.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan(basePackages = "gladiator")
-@PropertySource("classpath:properties/${properties.profile}/data-store.properties")
+@PropertySources({
+        @PropertySource("classpath:properties/${properties.profile}/data-store.properties"),
+        @PropertySource("classpath:properties/${properties.profile}/webapp.properties")
+})
 public class GlobalConfig {
 
     @Autowired
