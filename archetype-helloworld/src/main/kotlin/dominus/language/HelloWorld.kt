c28d45f601b4c2620116df3dfd4dbb371dd800de
guoxu1231 <guoxu1231@gmail.com>
2017-05-21 21:53:12 +0800

    add Kotlin support.

---
+++ archetype-helloworld/src/main/kotlin/dominus/language/HelloWorld.kt
@@ -0,0 +1,23 @@
+package dominus.language
+
+import org.junit.Test
+import origin.common.junit.DominusJUnit4TestBase
+import org.junit.Assert.*
+
+open class HelloWorld : DominusJUnit4TestBase() {
+    override fun doSetUp() {
+        super.doSetUp()
+        println("HelloWorld.doSetup")
+    }
+
+    override fun doTearDown() {
+        super.doTearDown()
+        println("HelloWorld.doTearDown")
+    }
+
+    @Test
+    fun testHelloWorld() {
+        assertTrue(true)
+    }
+
+}
