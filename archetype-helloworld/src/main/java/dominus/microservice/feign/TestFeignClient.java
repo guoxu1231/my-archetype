6cd94d0ac07cf1b6e6aa2f64d4ca4c140d5bfb0d
guoxu <guoxu1231@gmail.com>
2017-07-19 21:02:19 +0800

    add TestFeignClient.testRibbonClientLoadBalancer

--- archetype-helloworld/src/main/java/dominus/microservice/feign/TestFeignClient.java
+++ archetype-helloworld/src/main/java/dominus/microservice/feign/TestFeignClient.java
@@ -1,14 +1,18 @@
 package dominus.microservice.feign;
 
+import dominus.microservice.feign.GitHubClient.Contributor;
 import feign.Feign;
+import feign.Retryer;
 import feign.gson.GsonDecoder;
+import feign.ribbon.RibbonClient;
 import feign.slf4j.Slf4jLogger;
+import org.junit.Ignore;
 import org.junit.Test;
 import origin.common.junit.DominusJUnit4TestBase;
-import dominus.microservice.feign.GitHubClient.Contributor;
 
 import java.util.List;
 
+import static com.netflix.config.ConfigurationManager.getConfigInstance;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -34,5 +38,32 @@ public class TestFeignClient extends DominusJUnit4TestBase {
         assertTrue(contributors.size() > 0);
     }
 
+    @Ignore
+    public void testRibbonClientLoadBalancer() {
 
+        // The Sun HTTP Client retries all requests once on an IOException, which makes testing retry code harder than would
+        // be ideal. We can only disable it for post, so lets at least do that.
+        System.setProperty("sun.net.http.retryPost", "false");
+
+        final String clientName = "github";
+        getConfigInstance().setProperty(clientName + ".ribbon.listOfServers",
+                "https://api.github.com");
+//        getConfigInstance().setProperty(clientName + ".ribbon.MaxAutoRetries", 1);
+//        getConfigInstance().setProperty(clientName + ".ribbon.MaxAutoRetriesNextServer", 1);
+//        getConfigInstance().setProperty(clientName + ".ribbon.ReadTimeout", 2000);
+//        getConfigInstance().setProperty(clientName + ".ribbon.ConnectTimeout", 2000);
+
+        GitHubClient gitHubClient = Feign.builder()
+                .decoder(new GsonDecoder())
+                .logLevel(feign.Logger.Level.FULL).logger(new Slf4jLogger())
+                .retryer(Retryer.NEVER_RETRY)
+                .client(RibbonClient.create()).target(GitHubClient.class, "https://" + clientName);
+
+        // Fetch and print a list of the contributors to this library.
+        List<Contributor> contributors = gitHubClient.contributors("OpenFeign", "feign");
+        for (Contributor contributor : contributors) {
+            System.out.println(contributor.login + " (" + contributor.contributions + ")");
+        }
+        assertTrue(contributors.size() > 0);
+    }
 }
