3463ae6bfb8fd42cc72282f4a181333464ccad72
shawguo-cn <guoxu1231@gmail.com>
2018-01-14 23:41:17 +0800

    upgrade to storm 1.1.1 and storm-kafka integration 0.10.+

--- archetype-hello-stream-storm/src/jvm/dominus/stream/storm/topology/kafka/Kafka08SpoutTestTopology.java
+++ archetype-hello-stream-storm/src/jvm/dominus/stream/storm/topology/kafka/Kafka08SpoutTestTopology.java
@@ -1,70 +1,70 @@
-package dominus.stream.storm.topology.kafka;
-
-import dominus.stream.storm.bolt.DevNullBolt;
-import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
-import org.apache.storm.StormSubmitter;
-import org.apache.storm.generated.StormTopology;
-import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.utils.Utils;
-
-import java.util.UUID;
-
-/**
- * Storm Kafka Integration (0.8.x)
- * http://storm.apache.org/releases/1.1.1/storm-kafka.html
- */
-@Deprecated
-public class Kafka08SpoutTestTopology {
-
-    public static final int DEFAULT_SPOUT_NUM = 1;
-    public static final int DEFAULT_BOLT_NUM = 1;
-    public static final int DEFAULT_WORKER_NUM = 2;
-
-    // names
-    public static final String TOPOLOGY_NAME = "Kafka08SpoutTestTopology";
-    public static final String SPOUT_ID = "Kafka_Spout";
-    public static final String BOLT_ID = "Printer_Bolt";
-
-    public static StormTopology getTopology(String zkConnString, String topicName) {
-
-        // 1 -  Setup Kafka Spout   --------
-        BrokerHosts brokerHosts = new ZkHosts(zkConnString);
-        SpoutConfig spoutConfig = new SpoutConfig(brokerHosts, topicName, "/" + topicName, UUID.randomUUID().toString());
-        spoutConfig.scheme = new StringMultiSchemeWithTopic();
-        spoutConfig.ignoreZkOffsets = true;
-        KafkaSpout spout = new KafkaSpout(spoutConfig);
-
-        // 2 -   Printer Bolt   --------
-        DevNullBolt bolt = new DevNullBolt();
-
-        // 3 - Setup Topology  --------
-        TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout(SPOUT_ID, spout, DEFAULT_SPOUT_NUM).setNumTasks(10);
-        builder.setBolt(BOLT_ID, bolt, DEFAULT_BOLT_NUM)
-                .localOrShuffleGrouping(SPOUT_ID);
-
-        return builder.createTopology();
-    }
-
-    /**
-     * Copies text file content from sourceDir to destinationDir. Moves source files into sourceDir after its done consuming
-     */
-    public static void main(String[] args) throws Exception {
-        Config conf = new Config();
-        conf.setDebug(true);
-        if (args != null && args.length > 0) {
-            conf.setNumWorkers(DEFAULT_WORKER_NUM);
-            //  Submit to Storm cluster
-            StormSubmitter.submitTopologyWithProgressBar(TOPOLOGY_NAME, conf, getTopology(args[0], args[1]));
-        } else {
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology(TOPOLOGY_NAME, conf, getTopology("TEST-BDD-064:2181/kafka", "kafka_bolt_test"));
-            Utils.sleep(400000);
-            cluster.killTopology(TOPOLOGY_NAME);
-            cluster.shutdown();
-        }
-    }
-
-
-}
+//package dominus.stream.storm.topology.kafka;
+//
+//import dominus.stream.storm.bolt.DevNullBolt;
+//import org.apache.storm.Config;
+//import org.apache.storm.LocalCluster;
+//import org.apache.storm.StormSubmitter;
+//import org.apache.storm.generated.StormTopology;
+//import org.apache.storm.topology.TopologyBuilder;
+//import org.apache.storm.utils.Utils;
+//
+//import java.util.UUID;
+//
+///**
+// * Storm Kafka Integration (0.8.x)
+// * http://storm.apache.org/releases/1.1.1/storm-kafka.html
+// */
+//@Deprecated
+//public class Kafka08SpoutTestTopology {
+//
+//    public static final int DEFAULT_SPOUT_NUM = 1;
+//    public static final int DEFAULT_BOLT_NUM = 1;
+//    public static final int DEFAULT_WORKER_NUM = 2;
+//
+//    // names
+//    public static final String TOPOLOGY_NAME = "Kafka08SpoutTestTopology";
+//    public static final String SPOUT_ID = "Kafka_Spout";
+//    public static final String BOLT_ID = "Printer_Bolt";
+//
+//    public static StormTopology getTopology(String zkConnString, String topicName) {
+//
+//        // 1 -  Setup Kafka Spout   --------
+//        BrokerHosts brokerHosts = new ZkHosts(zkConnString);
+//        SpoutConfig spoutConfig = new SpoutConfig(brokerHosts, topicName, "/" + topicName, UUID.randomUUID().toString());
+//        spoutConfig.scheme = new StringMultiSchemeWithTopic();
+//        spoutConfig.ignoreZkOffsets = true;
+//        KafkaSpout spout = new KafkaSpout(spoutConfig);
+//
+//        // 2 -   Printer Bolt   --------
+//        DevNullBolt bolt = new DevNullBolt();
+//
+//        // 3 - Setup Topology  --------
+//        TopologyBuilder builder = new TopologyBuilder();
+//        builder.setSpout(SPOUT_ID, spout, DEFAULT_SPOUT_NUM).setNumTasks(10);
+//        builder.setBolt(BOLT_ID, bolt, DEFAULT_BOLT_NUM)
+//                .localOrShuffleGrouping(SPOUT_ID);
+//
+//        return builder.createTopology();
+//    }
+//
+//    /**
+//     * Copies text file content from sourceDir to destinationDir. Moves source files into sourceDir after its done consuming
+//     */
+//    public static void main(String[] args) throws Exception {
+//        Config conf = new Config();
+//        conf.setDebug(true);
+//        if (args != null && args.length > 0) {
+//            conf.setNumWorkers(DEFAULT_WORKER_NUM);
+//            //  Submit to Storm cluster
+//            StormSubmitter.submitTopologyWithProgressBar(TOPOLOGY_NAME, conf, getTopology(args[0], args[1]));
+//        } else {
+//            LocalCluster cluster = new LocalCluster();
+//            cluster.submitTopology(TOPOLOGY_NAME, conf, getTopology("TEST-BDD-064:2181/kafka", "kafka_bolt_test"));
+//            Utils.sleep(400000);
+//            cluster.killTopology(TOPOLOGY_NAME);
+//            cluster.shutdown();
+//        }
+//    }
+//
+//
+//}
---
+++ archetype-hello-stream-storm/src/jvm/dominus/stream/storm/topology/kafka/KafkaSpoutTestTopology.java
@@ -0,0 +1,76 @@
+package dominus.stream.storm.topology.kafka;
+
+import dominus.stream.storm.bolt.DevNullBolt;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.kafka.spout.KafkaSpoutConfig;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.utils.Utils;
+
+import java.util.UUID;
+
+/**
+ * Storm Kafka Integration (0.10.x+)
+ * http://storm.apache.org/releases/1.1.1/storm-kafka-client.html
+ *
+ * NOT COMPATIBLE with STORM 1.0.2
+ * java.lang.NoSuchMethodError: org.apache.storm.utils.Time.nanoTime()J at org.apache.storm.kafka.spout.internal.Timer.<init>(Timer.java:45) at org.apache.storm.kafka.spout.KafkaSpout.open(KafkaSpout.j
+ */
+public class KafkaSpoutTestTopology {
+    public static final int DEFAULT_SPOUT_NUM = 1;
+    public static final int DEFAULT_BOLT_NUM = 1;
+    public static final int DEFAULT_WORKER_NUM = 2;
+    public static final int DEFAULT_KAFKA_SPOUT_NUM = 2;
+
+    // names
+    public static final String TOPOLOGY_NAME = "Kafka10SpoutTestTopology";
+    public static final String SPOUT_ID = "New_Kafka_Spout";
+    public static final String BOLT_ID = "Printer_Bolt";
+
+    // default kafka config
+    public static final String BOOTSTRAP_SERVERS = "NM-304-HW-XH628V3-BIGDATA-089:9092";
+    public static final String KAFKA_TEST_TOPIC = "kafka_bolt_test";
+
+    public static StormTopology getTopology(String bootstrapServers, String... topics) {
+
+        // 1 -  Setup Kafka Spout   --------
+        KafkaSpoutConfig<String, String> kafkaConf = KafkaSpoutConfig.
+                builder(bootstrapServers, topics)
+                .setGroupId(UUID.randomUUID().toString())
+                .build();
+        KafkaSpout spout = new KafkaSpout(kafkaConf);
+
+        // 2 -   Printer Bolt   --------
+        DevNullBolt bolt = new DevNullBolt();
+
+        // 3 - Setup Topology  --------
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout(SPOUT_ID, spout, DEFAULT_SPOUT_NUM).setNumTasks(DEFAULT_KAFKA_SPOUT_NUM);
+        builder.setBolt(BOLT_ID, bolt, DEFAULT_BOLT_NUM)
+                .localOrShuffleGrouping(SPOUT_ID);
+
+        return builder.createTopology();
+    }
+
+    /**
+     * Copies text file content from sourceDir to destinationDir. Moves source files into sourceDir after its done consuming
+     */
+    public static void main(String[] args) throws Exception {
+        Config conf = new Config();
+        conf.setDebug(true);
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(DEFAULT_WORKER_NUM);
+            //  Submit to Storm cluster
+            StormSubmitter.submitTopologyWithProgressBar(TOPOLOGY_NAME, conf, getTopology(args[0], args[1]));
+        } else {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology(TOPOLOGY_NAME, conf, getTopology(BOOTSTRAP_SERVERS, KAFKA_TEST_TOPIC));
+            Utils.sleep(400000);
+            cluster.killTopology(TOPOLOGY_NAME);
+            cluster.shutdown();
+        }
+    }
+}
